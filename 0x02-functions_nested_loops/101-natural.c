40a4ef1c0865ced030a2243fdb373c21ecb1fb1b
Maleda <m3mahalla@gmail.com>
2022-04-13 11:00:09 -0700

    Nature made the natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,16 +6,16 @@
  *
  * Return: Always 0.
  */
-int main(void)
+
+	int main(void)
 {
-        int a, b;
+	int a,b;
 
-        for (a = 1; a < 1024; a++)
-        {
-                if ((a % 3) == 0 || (a % 5) == 0)
-                        b += a;
-        }
-        printf("%d\n", b);
-        return (0);
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
 }
-
