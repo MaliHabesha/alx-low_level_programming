dc89bd9c3d4ecd9256a9869e36c98b0c16f387bb
Mali <m3mahalla@gmail.com>
2022-04-14 02:04:42 -0700

    subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@
 
         i = 0;
 
-        while (i < 10)
-        {
-                putchar(i); /* this is the spot that generates loop*/
-        }
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
 
         printf("Infinite loop avoided! \\o/\n");
 
