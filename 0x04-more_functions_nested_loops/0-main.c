54bd4e7e5609924f5f62d25450f944570706a832
Mali <m3mahalla@gmail.com>
2022-04-14 00:27:38 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -6,15 +6,13 @@
  *
  * Return: Always 0.
  */
-
-int _isupper(int c)
+int main(void)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
 }
