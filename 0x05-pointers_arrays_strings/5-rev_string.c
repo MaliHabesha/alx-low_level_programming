8cfc6b92a41425dd3af31ef689cff1346729fe16
Mali <m3mahalla@gmail.com>
2022-04-18 09:38:23 -0700

    stylistic consequences

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - function with one argument
+ * @s: char type argument
+ *
+ * Description: reversing a string
+ * Return: na
+ */
+void rev_string(char *s)
+{
+        int count = 0, i, j;
+        char *str, temp;
+
+        while (count >= 0)
+        {
+                if (s[count] == '\0')
+                        break;
+                count++;
+        }
+        str = s;
+
+        for (i = 0; i < (count - 1); i++)
+        {
+                for (j = i + 1; j > 0; j--)
+                {
+                        temp = *(str + j);
+                        *(str + j) = *(str + (j - 1));
+                        *(str + (j - 1)) = temp;
+                }
+        }
+}
+
