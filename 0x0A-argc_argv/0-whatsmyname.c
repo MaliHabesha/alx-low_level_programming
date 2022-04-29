0edc465c933a4ca94924c4ed10bef115e70b27fb
Mali <m3mahalla@gmail.com>
2022-04-29 09:49:38 -0700

    what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ * main - function with two arguments
+ * @argc: int type argument
+ * @argv: char type pointer array argument
+ *
+ * Description: prints all arguments it recieves
+ * Return: void
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
